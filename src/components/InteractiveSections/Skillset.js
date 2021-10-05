02b48391ba9c4ad3dbd6da6f6e7d5a2951ea4f97
Amrit-S <aksingh@ucsd.edu>
2021-10-04 23:17:12 -0700

    adjusted ratings for skillset (content)

--- src/components/InteractiveSections/Skillset.js
+++ src/components/InteractiveSections/Skillset.js
@@ -24,23 +24,23 @@ export default function Skillset() {
                     <SkillEntry rating={4} title="GitHub/Git"/>
                     <SkillEntry rating={4} title="MongoDB"/>
                     <SkillEntry rating={4} title="Mongoose"/>
-                    <SkillEntry rating={4} title="Keras/Tensorflow"/>
                     <SkillEntry rating={4} title="Jupyter Notebooks"/>
-                    <SkillEntry rating={4} title="Node.js"/>
-                    <SkillEntry rating={4} title="jQuery"/>
-                    <SkillEntry rating={4} title="Bootstrap"/>
+                    <SkillEntry rating={3.5} title="Express"/>
+                    <SkillEntry rating={3.5} title="Node.js"/>
+                    <SkillEntry rating={3.5} title="Material UI"/>
+                    <SkillEntry rating={3} title="Bootstrap"/>
                 </div>
                 <div>
-                    <SkillEntry rating={4} title="Material UI"/>
-                    <SkillEntry rating={4} title="Figma"/>
-                    <SkillEntry rating={4} title="Express"/>
-                    <SkillEntry rating={4} title="Command Line"/>
-                    <SkillEntry rating={4} title="MySQL"/>
-                    <SkillEntry rating={4} title="SQLite"/>
-                    <SkillEntry rating={4} title="GDB"/>
-                    <SkillEntry rating={4} title="Valgrind"/>
-                    <SkillEntry rating={4} title="Vim"/>
-                    <SkillEntry rating={4} title="Android Studio"/>
+                    <SkillEntry rating={3} title="jQuery"/>
+                    <SkillEntry rating={3} title="Keras/Tensorflow"/>
+                    <SkillEntry rating={3} title="Figma"/>
+                    <SkillEntry rating={3} title="Command Line"/>
+                    <SkillEntry rating={3} title="GDB"/>
+                    <SkillEntry rating={3} title="Valgrind"/>
+                    <SkillEntry rating={2.5} title="MySQL"/>
+                    <SkillEntry rating={2.5} title="SQLite"/>
+                    <SkillEntry rating={2.5} title="Vim"/>
+                    <SkillEntry rating={2} title="Android Studio"/>
                 </div>
             </div>
             <hr/>
@@ -48,24 +48,24 @@ export default function Skillset() {
             <div className="skillset-subsection">
                 <div>
                     <SkillEntry rating={4} title="Matplotlib"/>
-                    <SkillEntry rating={4} title="CV2"/>
-                    <SkillEntry rating={4} title="NumPy"/>
+                    <SkillEntry rating={3.5} title="CV2"/>
+                    <SkillEntry rating={3} title="NumPy"/>
                 </div>
                 <div>
-                    <SkillEntry rating={4} title="PIL"/>
-                    <SkillEntry rating={4} title="Pandas"/>
+                    <SkillEntry rating={3} title="PIL"/>
+                    <SkillEntry rating={3} title="Pandas"/>
                 </div>
             </div>
             <hr/>
             <h4> Experience </h4>
             <div className="skillset-subsection">
                 <div>
-                    <SkillEntry rating={4} title="JSON"/>
-                    <SkillEntry rating={4} title="Agile SCRUM"/>
+                    <SkillEntry rating={4.5} title="JSON"/>
+                    <SkillEntry rating={4.5} title="Agile SCRUM"/>
                 </div>
                 <div>
-                    <SkillEntry rating={4} title="SOLID Principles"/>
-                    <SkillEntry rating={4} title="UML"/>
+                    <SkillEntry rating={3.5} title="SOLID Principles"/>
+                    <SkillEntry rating={3.5} title="UML"/>
                 </div>
             </div>
             <hr/>
@@ -74,24 +74,24 @@ export default function Skillset() {
             <h5 style={{marginTop: "0"}}> Technical </h5>
             <div className="skillset-subsection">
                 <div>
+                    <SkillEntry rating={4.5} title="Python"/>
                     <SkillEntry rating={4} title="HTML5"/>
                     <SkillEntry rating={4} title="CSS3"/>
-                    <SkillEntry rating={4} title="Python"/>
-                    <SkillEntry rating={4} title="JavaScript"/>
+                    <SkillEntry rating={3.5} title="JavaScript"/>
                 </div>
                 <div>
-                    <SkillEntry rating={4} title="Java"/>
-                    <SkillEntry rating={4} title="C++"/>
-                    <SkillEntry rating={4} title="SQL"/>
-                    <SkillEntry rating={4} title="C#"/>
+                    <SkillEntry rating={3.5} title="Java"/>
+                    <SkillEntry rating={3} title="C++"/>
+                    <SkillEntry rating={3} title="C#"/>
+                    <SkillEntry rating={2.5} title="SQL"/>
                 </div>
             </div>
             <hr style={{width: "75%", marginTop: "20px"}}/>
             <h5> Non-Technical </h5>
             <div className="skillset-subsection">
                 <div>
-                    <SkillEntry rating={4} title="English"/>
-                    <SkillEntry rating={4} title="Punjabi"/>
+                    <SkillEntry rating={5} title="English"/>
+                    <SkillEntry rating={5} title="Punjabi"/>
                 </div>
                 <div>
                     <SkillEntry rating={4} title="Hindi/Urdu"/>
